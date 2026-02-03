0aeadbc355e169fc1a2ce9a9fce2457e1562bdb8
sahilaggarwal2002 <44200191+sahilaggarwal2002@users.noreply.github.com>
2026-02-03 19:31:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
   
     //yes page
     valentineQuestion.innerHTML = `
-      <img src="./images/image7.gif" alt="Celebration duckie" style="display: block; margin: 0 auto; width: 200px; height: auto;"/>
+      <img src="./images/image7.jpeg" alt="Celebration duckie" style="display: block; margin: 0 auto; width: 200px; height: auto;"/>
       Congratulations!!<br>
       <span style="font-size: 20px; color: #bd1e59;">You have scored a baddie for Valentine's Day! <3</span>
     `;
@@ -156,3 +156,4 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
   }
 
   
+
