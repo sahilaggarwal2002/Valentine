00fddea90967671e6304c29da1c50f9df9efeee2
sahilaggarwal2002 <44200191+sahilaggarwal2002@users.noreply.github.com>
2026-02-03 19:35:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,16 +97,16 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
     valentineQuestion.style.textAlign = "center"; 
   
     //make image go boing
-    const bounceImage = document.createElement("img");
-    bounceImage.src = "./images/baddie.jpg";
-    bounceImage.alt = "Baddie";
-    bounceImage.style.position = "absolute";
-    bounceImage.style.width = "300px";
-    bounceImage.style.height = "325px";
-    bounceImage.style.borderRadius = "50%";
-    document.body.appendChild(bounceImage);
+   // const bounceImage = document.createElement("img");
+  //  bounceImage.src = "./images/baddie.jpg";
+ //   bounceImage.alt = "Baddie";
+  //  bounceImage.style.position = "absolute";
+   // bounceImage.style.width = "300px";
+   // bounceImage.style.height = "325px";
+  //  bounceImage.style.borderRadius = "50%";
+//    document.body.appendChild(bounceImage);
   
-    startBouncing(bounceImage);
+  //  startBouncing(bounceImage);
   
     confetti({
       particleCount: 150,
@@ -156,5 +156,6 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
   }
 
   
+
 
 
